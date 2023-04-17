f9a61df40d16bd95d61bd3656b81a7c01b9c2325
dsjordao <jordaodaniel@hotmail.com>
2023-04-17 11:04:47 -0300

    finalizando aula compilador

---
+++ 001 - Inicio/006 - Compilador/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Server {
+    constructor() {
+        console.log(1);
+    }
+}
+new Server();
+//# sourceMappingURL=index.js.map
